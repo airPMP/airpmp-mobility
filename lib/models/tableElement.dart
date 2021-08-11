3ffaa5f4df20394ea2417d3241c64b4e2d04b859
AthulJohn <athulpulickaljohn@gmail.com>
2021-08-11 17:42:12 +0530

    improved table ui

--- lib/models/tableElement.dart
+++ lib/models/tableElement.dart
@@ -1,12 +1,16 @@
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class TableElement {
   String valueName = "";
+  Color color = Colors.black;
   int flex = 1;
   int maxLines = 2;
   TextAlign textAlign = TextAlign.center;
   TableElement(this.valueName,
-      {this.flex = 1, this.maxLines = 2, this.textAlign = TextAlign.center});
+      {this.flex = 1,
+      this.maxLines = 2,
+      this.textAlign = TextAlign.center,
+      this.color = Colors.black});
 }
 
 class TableValueElement extends TableElement {
@@ -16,6 +20,8 @@ class TableValueElement extends TableElement {
       {flex = 1,
       maxLines = 2,
       textAlign = TextAlign.center,
-      this.useCustomflex = false})
-      : super(valueName, flex: flex, maxLines: maxLines, textAlign: textAlign);
+      this.useCustomflex = false,
+      color = const Color(0xff545454)})
+      : super(valueName,
+            flex: flex, maxLines: maxLines, textAlign: textAlign, color: color);
 }
