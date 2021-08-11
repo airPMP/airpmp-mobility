52a9555d718c297ac28368b2355f3ff12a1ea941
k <k@k.c0m>
2021-08-11 20:50:53 +0530

    minor update

--- lib/utilities/constants/fonts.dart
+++ lib/utilities/constants/fonts.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:sizer/sizer.dart';
 
 abstract class CustomTextStyles {
   static const TextStyle title =
       TextStyle(fontWeight: FontWeight.bold, fontSize: 25);
-  static const whiteTitle =
-      TextStyle(fontSize: 25, color: Colors.white, fontWeight: FontWeight.bold);
+  static const TextStyle subtitle =
+      TextStyle(fontWeight: FontWeight.w600, fontSize: 16);
+  static const TextStyle whiteSubtitle =
+      TextStyle(fontWeight: FontWeight.w600, fontSize: 16, color: Colors.white);
+  static const whiteTitle = TextStyle(
+      fontSize: 25.0, color: Colors.white, fontWeight: FontWeight.bold);
   static const TextStyle listTileTitle = TextStyle();
   static const TextStyle appbarTitle = TextStyle();
   static const TextStyle buttonText =
