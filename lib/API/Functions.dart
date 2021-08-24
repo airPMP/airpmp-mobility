0e0c5ed53be480ca0bd25d0e1fd13ec3ea78231c
Kiran S Baliga <kiransbaliga@gmail.com>
2021-08-24 23:45:34 +0530

    Funstions updated

--- lib/API/Functions.dart
+++ lib/API/Functions.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 import 'dart:convert';
+import 'dart:ffi';
 import 'package:airpmp_mobility/Constants/Classes.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart';
@@ -45,3 +46,140 @@ FutureOr<String> getToken() async {
       await secureStorage.readSecureData("token") ?? "Token is invalid";
   return token;
 }
+
+FutureOr<void> saveCompanyId(String companyId) async {
+  secureStorage.writeSecureData("companyId", companyId);
+  print("Company Id has been saved");
+}
+
+FutureOr<String> getCompanyId() async {
+  String companyId = await secureStorage.readSecureData("companyId") ??
+      "Company Id is invalid";
+  return companyId;
+}
+
+FutureOr<void> saveProjectId(String userId) async {
+  secureStorage.writeSecureData("projectId", userId);
+  print("Project Id has been saved");
+}
+
+FutureOr<String> getProjectId() async {
+  String projectId = await secureStorage.readSecureData("projectId") ??
+      "Project Id is invalid";
+  return projectId;
+}
+
+FutureOr<void> saveUserId(String userId) async {
+  secureStorage.writeSecureData("userId", userId);
+  print("User Id has been saved");
+}
+
+FutureOr<String> getUserId() async {
+  String userId =
+      await secureStorage.readSecureData("userId") ?? "User Id is invalid";
+  return userId;
+}
+
+//<==============Get Project Details=================>
+
+Future<List<MyProject>?> getProjects(String token, String companyID) async {
+  var projectsJson;
+  var aProject;
+  List<MyProject> listOfProjects = [];
+
+  print('getting list of projects...');
+  String url =
+      "https://airpmo.herokuapp.com/api/projects/index?search=&company=" +
+          companyID;
+  Map<String, String> headers = {
+    "Content-type": "application/json",
+    'Accept': 'application/json',
+    "Authorization": "Bearer " + token,
+  };
+  Response response = await get(url, headers: headers);
+  print(response.body);
+
+  if (response.statusCode == 200) {
+    print('sucessfully obtained projects....');
+    projectsJson = jsonDecode(response.body);
+    for (aProject in projectsJson) {
+      listOfProjects.add(MyProject.fromJson(aProject));
+    }
+    int len = listOfProjects.length;
+    print("number of projects = $len");
+    return listOfProjects;
+  } else
+    return null;
+}
+Future<List<MyJobCard>> getInProgressJobCard(String token) async {
+  print('geting in progress jsc');
+  List<MyJobCard> inProgressList = [];
+  dynamic allJC = await getMyJobCard(token);
+  // print(allJC.length);
+  allJC = allJC['myJobCardList'];
+
+  for (MyJobCard aJC in allJC) {
+    if (aJC.jcStatus == "In-Progress") {
+      inProgressList.add(aJC);
+    }
+  }
+  return inProgressList;
+}
+
+Future<List<MyJobCard>> getExecutedJobCard(String token) async {
+  print('geting in progress jsc');
+  List<MyJobCard> inProgressList = [];
+  dynamic allJC = await getMyJobCard(token);
+  allJC = allJC['myJobCardList'];
+  // print(allJC.length);
+  for (MyJobCard aJC in allJC) {
+    if (aJC.jcStatus == "Executed") {
+      inProgressList.add(aJC);
+    }
+  }
+  return inProgressList;
+}
+
+Future<Map<String, dynamic>?> getMyJobCard(String token) async {
+  print('getting job card from internet..');
+  var myJobCardsJson;
+  var aJobCard;
+  final String userID = await getUserId();
+  final String projectID = await getProjectId();
+  List<MyJobCard> myJobCardsList = [];
+
+  // -------------this will only get sama al jadaf 's job cards -------------------
+  print(userID);
+  print(projectID);
+  String url =
+      'https://airpmo.herokuapp.com/api/jobcard/index?search=&userId=$userID&projectId=$projectID';
+  Map<String, String> headers = {
+    "Content-type": "application/json",
+    'Accept': 'application/json',
+    "Authorization": "Bearer " + token,
+  };
+  try {
+    Response response = await get(url, headers: headers);
+    print(response.statusCode);
+    print(response.body);
+
+    if (response.statusCode == 200) {
+      print('sucessfully obtained jobcards....');
+      myJobCardsJson = jsonDecode(response.body);
+      for (aJobCard in myJobCardsJson) {
+        myJobCardsList.add(MyJobCard.fromJson(aJobCard));
+      }
+    }
+    int len = myJobCardsList.length;
+    print("number of jc in obtained = $len");
+    // return myJobCardsList;
+    return {
+      'myJobCardList': myJobCardsList,
+      'numberOfJCs': myJobCardsList.length
+    };
+  } catch (e) {
+    print(e);
+    return null;
+  }
+}
+
--- lib/Constants/Classes.dart
+++ lib/Constants/Classes.dart
@@ -1,11 +1,11 @@
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
 class LoginDetails {
- 
   String userid;
   String token;
   String company;
-  LoginDetails({required this.userid, required this.token, required this.company});
+  LoginDetails(
+      {required this.userid, required this.token, required this.company});
 
   static LoginDetails fromJson(jsonResponse) {
     return LoginDetails(
@@ -16,18 +16,62 @@ class LoginDetails {
   }
 }
 
-class SecureStorage{
+class SecureStorage {
   final _storage = FlutterSecureStorage();
-  Future writeSecureData(String key, String value)  async {
+  Future writeSecureData(String key, String value) async {
     var writeData = await _storage.write(key: key, value: value);
     return writeData;
   }
+
   Future readSecureData(String key) async {
     var readData = await _storage.read(key: key);
     return readData;
   }
-  Future deleteSecureData(String key) async{
+
+  Future deleteSecureData(String key) async {
     var deleteData = await _storage.delete(key: key);
     return deleteData;
+  }
+}
+
+// <==============================================================>
+
+class MyJobCard {
+  // MyJobCard({this.activiyName, this.zone});
+  late String jobCardNumber;
+  late String activiyName;
+  late String zone;
+  late String jcStatus;
+  late String activityCode;
+  // assingedDate is createdDate
+  late String assignedDate;
+  late String tobeAchievedQTY;
+ late DateTime convertedCreatedDateTime;
+
+  MyJobCard.fromJson(Map<String, dynamic> json) {
+    jobCardNumber = json['_id'];
+    activiyName = json['activityName'];
+    zone = json['zone'];
+    jcStatus = json['JCStatus'];
+    activityCode = json['activityCode'];
+    assignedDate = json['assignedDate'];
+    tobeAchievedQTY = json['tobeAchievedQTY'];
+    convertedCreatedDateTime = DateTime.parse(assignedDate);
+    
+  }
+}
+// <==============================================================>
+
+
+class MyProject {
+  MyProject({required this.name, required this.id, required this.active});
+  late String id;
+  late String name;
+  late bool active;
+
+  MyProject.fromJson(Map<String, dynamic> json) {
+    name = json['name'];
+    id = json['_id'];
+    active = json['active'];
   }
 }
