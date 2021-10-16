12b93c9618be3a194e7ff95cc5027f6beeeed9da
AthulJohn <athulpulickaljohn@gmail.com>
2021-10-16 10:38:30 +0530

    solved a minor error

--- lib/main.dart
+++ lib/main.dart
@@ -8,35 +8,31 @@ import 'package:provider/provider.dart';
 import 'API/Functions.dart';
 import 'Screens/login.dart';
 
-
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        debugShowCheckedModeBanner: false,
-        theme: ThemeData(
-          fontFamily: "Rubik",
-          inputDecorationTheme: InputDecorationTheme(),
-          floatingActionButtonTheme:
-              FloatingActionButtonThemeData(backgroundColor: Color(0xFFF4442E)),
-          appBarTheme: AppBarTheme(
-            color: Color(0xFFF4442E),
-            iconTheme: IconThemeData(color: Colors.black),
-            textTheme: TextTheme(
-              headline1: TextStyle(color: Colors.black),
-            ),
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        fontFamily: "Rubik",
+        inputDecorationTheme: InputDecorationTheme(),
+        floatingActionButtonTheme:
+            FloatingActionButtonThemeData(backgroundColor: Color(0xFFF4442E)),
+        appBarTheme: AppBarTheme(
+          color: Color(0xFFF4442E),
+          iconTheme: IconThemeData(color: Colors.black),
+          textTheme: TextTheme(
+            headline1: TextStyle(color: Colors.black),
           ),
         ),
-        home:getToken()=="" ? LoginPage():ChangeNotifierProvider<ProviderModel>(
-                                      create: (context) => ProviderModel(log),
-                                      child: MainApp(
-                                          isTab: MediaQuery.of(context)
-                                                  .size
-                                                  .width >
-                                              700)),
-        );
+      ),
+      home: getToken() == ""
+          ? LoginPage()
+          : ChangeNotifierProvider<ProviderModel>(
+              create: (context) => ProviderModel(null),
+              child: MainApp(isTab: MediaQuery.of(context).size.width > 700)),
+    );
   }
 }
-
